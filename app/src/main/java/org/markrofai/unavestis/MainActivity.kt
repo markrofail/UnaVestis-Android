5398704d90ea071dee957208c65c74851f02287d
Mark Rofail <markm.rofail@gmail.com>
2019-12-08 19:52:11 +0200

    initial commit

---
+++ app/src/main/java/org/markrofai/unavestis/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.markrofai.unavestis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
